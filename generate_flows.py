35ca71fbce201ac229f03bb5c551acd8a1151d13
Jacobo Rdz <jacoborzt@gmail.com>
2025-10-17 16:28:27 -0600

    feat: create an auxiliar function to make a test

--- generate_flows.py
+++ generate_flows.py
@@ -6,7 +6,7 @@ import json
 # eth_type: the ethernet type, 2048 for IPv4 and 2054 for ARP protocol
 # ip_dst: the IPv4 destination in a string, for example: '0.0.0.0/32' if it is setted to 0 then the field its not setted.
 # out_node: the output node it could be a number or a string like: 'NORMAL'
-def flow(id, priority, eth_type, out_port, in_port=False, ip_src=False, ip_dst=False):
+def flow(id, priority, eth_type, out_port, in_port='', ip_src='', ip_dst=''):
     flow = {
         "flow": [
             {
@@ -30,7 +30,7 @@ def flow(id, priority, eth_type, out_port, in_port=False, ip_src=False, ip_dst=F
                                     {
                                         "order": 0,
                                         "output-action": {
-                                            "output-node-connector": out_port
+                                            "output-node-connector": str(out_port)
                                         }
                                     }
                                 ]
@@ -41,32 +41,56 @@ def flow(id, priority, eth_type, out_port, in_port=False, ip_src=False, ip_dst=F
             }
         ]
     }
-    if ip_src:
+    if ip_src != '':
         flow['flow'][0]['match']['ipv4-source'] = ip_src
-    if ip_dst:
+    if ip_dst !='':
         flow['flow'][0]['match']['ipv4-destination'] = ip_dst
-    if in_port:
+    if in_port != '':
         flow['flow'][0]['match']['in-port'] = f'openflow:{id // 10}:{in_port}'
+    print(json.dumps(flow, indent=2))
     return flow
 
 def load_flow(flow, id, c_ip='172.17.0.2'):
     id_flow = flow['flow'][0]['id']
-    endpoint = f'http://{c_ip}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{id}/flow-node-inventory:table=0/flow={id_flow}'
-    r = requests.request(method='PUT', json=json.dumps(flow, indent=2), url=endpoint, auth=('admin', 'admin'), headers={'Content-Type': 'application/json'})
-    print(r.status_code)
+    url = f'http://{c_ip}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{id}/flow-node-inventory:table=0/flow={id_flow}'
+    req = requests.request(method='PUT', json=flow, url=url, auth=('admin', 'admin'), headers={'Content-Type': 'application/json'})
+    print(f'{id_flow}: {req.status_code}')
 
 def upload_all(n_switch=1, n_host=3):
-    load_flow(flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL'), 1)      # Flow for the ARP protocol in s0
-    load_flow(flow(id='11', priority=9, eth_type=2048, ip_dst='10.0.0.1/32', out_port=1), 1)   # Flow from s0 to the server
-    load_flow(flow(id='12', priority=9, eth_type=2048, ip_dst='10.0.0.2/32', out_port=2), 1)   # Flow from s0 to L
-    load_flow(flow(id='13', priority=3, eth_type=2048, ip_dst='10.0.0.0/16', out_port=3), 1)   # Flow for the 'internet'
+    load_flow(flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL'), 1)                  # Flow for the ARP protocol in s0
+    load_flow(flow(id='11', priority=9, eth_type=2048, ip_dst='10.0.0.1/32', out_port=1), 1)    # Flow from s0 to the server
+    load_flow(flow(id='12', priority=9, eth_type=2048, ip_dst='10.0.0.2/32', out_port=2), 1)    # Flow from s0 to L
+    load_flow(flow(id='13', priority=3, eth_type=2048, ip_dst='10.0.0.0/16', out_port=3), 1)    # Flow for the 'internet'
 
     for switch in range(1, n_switch + 1):
-        print(f'Loading flows for the switch {switch}')
-        load_flow(flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL'), switch+1)
-        load_flow(flow(id=f'{switch+1}0', priority=3, eth_type=2048, ip_dst='10.0.0.0/24', out_port=1), switch+1)
+        # Create the ARP flow for each switch
+        arp = flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL')
+        load_flow(arp, switch+1)
+        # flow to the server
+        srv = flow(id=f'{switch+1}0', priority=3, eth_type=2048, ip_dst='10.0.0.1/24', out_port=1)
+        load_flow(srv, switch+1)
         for host in range(1, n_host + 1):
-            print(f'\thost {switch}{host}', end=' ')
-            load_flow(flow(id=f'{switch+1}{host}', priority=5, eth_type=2048, ip_dst=f'10.0.{switch}.{host}/32', out_port=host+1), switch+1)
+            host_flow = flow(id=f'{switch+1}{host}', priority=5, eth_type=2048, ip_dst=f'10.0.0.{switch}{host}/32', out_port=host+1)
+            load_flow(host_flow, switch+1)
 
-upload_all()
+def test_second_experiment():
+    # Protocolos ARP
+    for i in range(1, 4):
+        load_flow(flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL'), i+1)
+
+    load_flow(flow(id=11, priority=1000, eth_type=2048, in_port='1', out_port=2), 1)
+    load_flow(flow(id=12, priority=1000, eth_type=2048, in_port='2', out_port=1), 1)
+    load_flow(flow(id=21, priority=1000, eth_type=2048, in_port='1', out_port=2), 2)
+    load_flow(flow(id=22, priority=1000, eth_type=2048, in_port='2', out_port=1), 2)
+    load_flow(flow(id=31, priority=8, eth_type=2048, ip_src='10.0.0.1/32', out_port=1), 3)
+    load_flow(flow(id=32, priority=8, eth_type=2048, ip_src='10.0.0.2/32', out_port=2), 3)
+    load_flow(flow(id=33, priority=9, eth_type=2048, ip_src='10.0.0.1/32', out_port=3), 3)
+    load_flow(flow(id=34, priority=9, eth_type=2048, ip_src='10.0.0.2/32', out_port=4), 3)
+    load_flow(flow(id=41, priority=8, eth_type=2048, ip_src='10.0.0.3/32', out_port=1), 4)
+    load_flow(flow(id=42, priority=8, eth_type=2048, ip_src='10.0.0.4/32', out_port=2), 4)
+    load_flow(flow(id=43, priority=9, eth_type=2048, ip_src='10.0.0.3/32', out_port=3), 4)
+    load_flow(flow(id=44, priority=9, eth_type=2048, ip_src='10.0.0.4/32', out_port=4), 4)
+
+
+# test_second_experiment()
+flow(id=11, priority=1000, eth_type=2048, in_port='1', out_port=2)
