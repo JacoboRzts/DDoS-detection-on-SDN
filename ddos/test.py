1eb76bb0e5e2f7b0392536a9cbce51968b08204c
Jacobo Rdz <jacoborzt@gmail.com>
2025-10-26 16:42:08 -0600

    remove the odl file

--- ddos/test.py
+++ ddos/test.py
@@ -3,28 +3,26 @@
 from mininet.log import setLogLevel
 import networks
 
-def test(n_hosts=3, seconds=10):
-    net = networks.tree(n_hosts)
+def test(n_switch=2, k_hosts=2, seconds=10):
+    net = networks.tree(n_switch, k_hosts)
 
-    server  = net.get('server')
-    listener = net.get('L')
-    s0 = net.get('s0')
+    victim = net.get('h01')
+    listner = net.get('h02')
 
+    switch = []
     hosts = []
 
-    for i in range(n_hosts):
-        hosts.append(net.get(f'h{i + 1}') )
-
-    file = f'ddos_snif.pcap'
-
-    print(f'Starting the test with {n_hosts}')
+    for i in range(1, n_switch+1):
+        switch.append(net.get(f's{i}'))
+        for j in range(1, k_hosts+1):
+            hosts.append(net.get(f'h{i-1}{j}'))
 
     # Start the traffic snif
-    s0.cmd(f'timeout {seconds + 1} tshark -i s0-eth1 -f "dst host {server.IP()} and not arp" -w {file}')
+    s1.cmd(f'timeout {seconds + 1} tshark -i s1-eth1 -f "dst host {victim.IP()} and not arp" -w ddos_snif.pcap')
 
     # Start the attack from N hosts
     for host in hosts:
-        host.cmd(f'timeout {seconds} hping3 -S -p 80 --rand-source -d 1000 --faster {server.IP()} &')
+        host.cmd(f'timeout {seconds} hping3 -S -p 80 --rand-source -d 1000 --faster {victim.IP()} &')
 
     net.stop()
 
