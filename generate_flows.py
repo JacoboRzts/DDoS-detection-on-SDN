d764989b68bae3a05b8c34133feee1cd0ca7ab07
Jacobo Rdz <jacoborzt@gmail.com>
2025-10-26 14:15:15 -0600

    feat: adds a function to load a basic tree flows

--- generate_flows.py
+++ generate_flows.py
@@ -52,36 +52,31 @@ def define_flow(id, priority, eth_type, out_port, in_port='', ip_src='', ip_dst=
         flow['flow'][0]['match']['in-port'] = f'openflow:{switch}:{in_port}'
     return flow
 
-def load_flow(flow, id, c_ip='172.17.0.2'):
+def load_flow(flow, id):
     id_flow = flow['flow'][0]['id']
-    url = f'http://{c_ip}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{id}/flow-node-inventory:table=0/flow={id_flow}'
+    url = f'http://{CONTROLLER_IP}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{id}/flow-node-inventory:table=0/flow={id_flow}'
     req = requests.request(method='PUT', json=flow, url=url, auth=('admin', 'admin'), headers={'Content-Type': 'application/json'})
     print(f'{id_flow}: {req.status_code}')
     # print(json.dumps(flow, indent=2))
 
-def remove_flow(flow, node, verbose=False):
-    url = f'http://{CONTROLLER_IP}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{node}/flow-node-inventory:table=0/flow={flow}'
-    req = requests.request(method='DELETE', url=url, auth=AUTH)
-    if verbose:
-        print(req.status_code)
+def clean_flows():
+    for i in range(1, 10):
+        req = requests.get(auth=AUTH, url=f'http://{CONTROLLER_IP}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{i}/flow-node-inventory:table=0')
+        if req.status_code // 100 == 2:
+            flows = req.json()['flow-node-inventory:table'][0]
+            if 'flow' in flows:
+                for flow in flows['flow']:
+                    rd = requests.delete(auth=AUTH, url=f'http://{CONTROLLER_IP}:8181/rests/data/opendaylight-inventory:nodes/node=openflow:{i}/flow-node-inventory:table=0/flow={flow["id"]}')
+                    print(flow['id'], rd.status_code)
 
+def simple_test(n_switch=1, n_host=2):
+    for i in range(1, n_switch+1):
+        load_flow(define_flow(id=f'{i}0', priority=4, eth_type=2054, out_port='NORMAL'), i)    # ARP
+        load_flow(define_flow(id=f'{i}1', priority=9, eth_type=2048, out_port='NORMAL'), i)    # Normal IPv4 Flow
+        if i > 1:
+            load_flow(define_flow(id=f'{i}2', priority=1, eth_type=2048, ip_dst='10.0.0.0/24', out_port='1'), i)    # Flow to the main network
+            load_flow(define_flow(id=f'1{i}', priority=1, eth_type=2048, ip_dst=f'10.0.{i-1}.0/24', out_port=f'{n_host+1}'), 1)    # Flow from the main network
 
-def upload_all(n_switch=1, n_host=3):
-    load_flow(flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL'), 1)                  # Flow for the ARP protocol in s0
-    load_flow(flow(id='11', priority=9, eth_type=2048, ip_dst='10.0.0.1/32', out_port=1), 1)    # Flow from s0 to the server
-    load_flow(flow(id='12', priority=9, eth_type=2048, ip_dst='10.0.0.2/32', out_port=2), 1)    # Flow from s0 to L
-    load_flow(flow(id='13', priority=3, eth_type=2048, ip_dst='10.0.0.0/16', out_port=3), 1)    # Flow for the 'internet'
-
-    for switch in range(1, n_switch + 1):
-        # Create the ARP flow for each switch
-        arp = flow(id='ARP', priority=4, eth_type=2054, out_port='NORMAL')
-        load_flow(arp, switch+1)
-        # flow to the server
-        srv = flow(id=f'{switch+1}0', priority=3, eth_type=2048, ip_dst='10.0.0.1/24', out_port=1)
-        load_flow(srv, switch+1)
-        for host in range(1, n_host + 1):
-            host_flow = flow(id=f'{switch+1}{host}', priority=5, eth_type=2048, ip_dst=f'10.0.0.{switch}{host}/32', out_port=host+1)
-            load_flow(host_flow, switch+1)
 
 def test_second_experiment():
     # Protocolos ARP
@@ -104,4 +99,7 @@ def test_second_experiment():
     load_flow(define_flow(id=43, priority=9, eth_type=2048, ip_dst='10.0.0.3/32', out_port=3), 4)
     load_flow(define_flow(id=44, priority=9, eth_type=2048, ip_dst='10.0.0.4/32', out_port=4), 4)
 
-test_second_experiment()
+print('Removing old flows')
+clean_flows()
+print('Upload new flows')
+simple_test(n_switch=2, n_host=3)
