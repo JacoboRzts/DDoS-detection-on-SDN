8164f5d49d498302a71701e1694eba11a98f7e4e
Jacobo Rdz <jacoborzt@gmail.com>
2025-10-28 14:08:48 -0600

    fix: remove the comments

--- generate_flows.py
+++ generate_flows.py
@@ -1,6 +1,10 @@
 import flows
 
-def test_second_experiment():
+def second_experiment_flows():
+    print('Removing old flows')
+    flows.cleanAll()
+    print('Upload new flows')
+
     # Protocolos ARP
     for i in range(2, 5):
         flows.load(flows.define(id=f'{i}0', priority=4, eth_type=2054, out_port='NORMAL'), i)
@@ -21,15 +25,13 @@ def test_second_experiment():
     flows.load(flows.define(id=43, priority=9, eth_type=2048, ip_dst='10.0.0.3/32', out_port=3), 4)
     flows.load(flows.define(id=44, priority=9, eth_type=2048, ip_dst='10.0.0.4/32', out_port=4), 4)
 
-def test_tree(n_switch=1, n_host=2):
+def tree_flows(n_switch=1, n_host=2):
+    print('Removing old flows')
+    flows.cleanAll()
+    print('Upload new flows')
     for i in range(1, n_switch+1):
         flows.load(flows.define(id=f'{i}0', priority=4, eth_type=2054, out_port='NORMAL'), i)    # ARP
         flows.load(flows.define(id=f'{i}1', priority=9, eth_type=2048, out_port='NORMAL'), i)    # Normal IPv4 Flow
         if i > 1:
             flows.load(flows.define(id=f'{i}2', priority=1, eth_type=2048, ip_dst='10.0.0.0/16', out_port='1'), i)    # Flow to the main network
             flows.load(flows.define(id=f'1{i}', priority=1, eth_type=2048, ip_dst=f'10.0.{i-1}.0/16', out_port=f'{n_host+1}'), 1)    # Flow from the main network
-
-print('Removing old flows')
-flows.cleanAll()
-print('Upload new flows')
-test_tree(n_switch=2, n_host=3)
