6f10e2f4247dd429728f0f71b691580b9d2f170c
Jacobo Rdz <jacoborzt@gmail.com>
2025-11-02 22:50:26 -0600

    fix: simplify the tree network

--- networks.py
+++ networks.py
@@ -23,13 +23,14 @@ def tree(n_switch=1, n_host=2):
     net = Mininet(controller=RemoteController, switch=OVSSwitch, link=TCLink)
     c1 = net.addController('c1', controller=RemoteController, ip=CONTROLLER_IP, port=CONTROLLER_PORT)
     switch_array = []
+    switch_array.append(net.addSwitch('s1', protocols="OpenFlow13"))
+    net.addLink(switch_array[0], net.addHost(f'srv', ip=f'10.0.0.1/16'), bw=4)
 
-    for i in range(n_switch):
+    for i in range(1, n_switch):
         switch_array.append(net.addSwitch(f's{i+1}', protocols="OpenFlow13"))
-        if i > 0:
-            net.addLink(switch_array[i], switch_array[0], bw=10)
+        net.addLink(switch_array[i], switch_array[0], bw=8)
         for j in range(n_host):
-            net.addLink(switch_array[i], net.addHost(f'h{i}{j+1}', ip=f'10.0.{i}.{j+1}/16'), bw=10)
+            net.addLink(switch_array[i], net.addHost(f'h{i}{j+1}', ip=f'10.0.{i}.{j+1}/16'), bw=8)
 
     net.build()
     c1.start()
