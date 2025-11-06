1497fe71138328c783ef65d7e1d19613b22086ce
Jacobo Rdz <jacoborzt@gmail.com>
2025-11-06 13:27:27 -0600

    feat: now can select the attack type (ICMP or SYN)

--- simulate.py
+++ simulate.py
@@ -3,24 +3,24 @@ from mininet.cli import CLI
 import networks
 import sys
 
-def ddos(n_switch=2, k_hosts=2, seconds=10):
+def ddos(n_switch=2, k_hosts=2, type='icmp'):
     setLogLevel('output')
     net = networks.tree(n_switch, k_hosts)
 
     victim = net.get('server')
 
     # Start the simple web server
-    print(f'Starting web server from server with IP {victim.IP()}')
+    print(f'Starting web server from server with IP {victim.IP()}:80')
     victim.cmd(f'python3 -m http.server 80 &')
 
     hosts = net.hosts
     for host in hosts[1:-1]:
-        print(f'Starting DoS attack from {host.name} to {victim.IP()}')
-        # SYN
-        host.cmd(f'hping3 -S --rand-source --flood -d 2048 {victim.IP()} &')
-        # ICMP
-        # host.cmd(f'hping3 -1 --rand-source --flood -d 4000 {victim.IP()} &')
-
+        if not str(k_hosts) in host.name:
+            if type == 'syn':
+                host.cmd(f'hping3 -S --rand-source --flood -d 2048 {victim.IP()} &')
+            else:
+                host.cmd(f'hping3 -1 --rand-source --flood -d 2048 {victim.IP()} &')
+            print(f'{type.upper()} DoS attack started from {host.name}')
     CLI(net)
     net.stop()
 
@@ -31,8 +31,9 @@ def normal(n_switch=2, n_host=3):
     net.stop()
 
 def conectivity(n_switch=2, n_host=3):
-    setLogLevel('info')
+    setLogLevel('output')
     net = networks.tree(n_switch, n_host)
+    net.pingAll()
     CLI(net)
     net.stop()
 
@@ -44,10 +45,16 @@ if __name__ == '__main__':
         hosts = int(sys.argv[3])
 
         match type:
+            case 'ping':
+                conectivity(switchs, hosts)
             case 'normal':
                 normal(switchs, hosts)
             case 'ddos':
-                ddos(switchs, hosts)
+                if len(sys.argv) > 4:
+                    type = sys.argv[4]
+                    ddos(switchs, hosts, type)
+                else:
+                    ddos(switchs, hosts)
             case _:
                 print('No se selecciono ningun tipo de test')
     else:
